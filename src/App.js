7566b81f5d62ad17303b9976bd4cba010c61430f
sebasvisser <sebas.visser@mensenrelatie.nl>
2023-02-01 17:45:35 +0100

    git weer gefixt denk ik

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import './App.css';
+
+function App() {
+  return (
+   <>
+       <header>
+           <h1>Weekend Escapes</h1>
+           <nav>
+               <a href="https://google.com">one word</a> |
+               <a href="https://google.com">one word</a> |
+               <a href="https://google.com">one word</a> |
+               <a href="https://google.com">one word</a>
+           </nav>
+       </header>
+       <main>
+           <header>
+               <h1>five words</h1>
+           </header>
+           <section>
+               <h2>three words</h2>
+               <p>forty-six words</p>
+               <p>forty-four words</p>
+           </section>
+           <section>
+               <h2>seven words</h2>
+               <p>sixty-eight words</p>
+               <p>forty-four words</p>
+           </section>
+       </main>
+       <footer>
+           <p>five words</p>
+       </footer>
+   </>
+  );
+}
+
+export default App;
