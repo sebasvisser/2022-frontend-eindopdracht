7d6ce81e10406a8b9b4152d78585b9110538ca2d
sebasvisser <sebas.visser@mensenrelatie.nl>
2023-02-01 23:36:54 +0100

    zoekpagina en formulier aangemaakt, werkt nog niet

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,40 +1,45 @@
 import React
     from 'react';
-import {
-    Link
-} from 'react-router-dom';
 
 function Search() {
     return (
         <>
             <header>
-                <h1>Search EPAGE</h1>
+                <h1>Zoeken, zoeken, zoeken</h1>
+                <h3>Vinden, vinden, vinden, vinden vinden, vinden, vinden vinden, vinden, vinden.</h3>
             </header>
             <main>
-                <header>
-                    <h1>five
-                        words</h1>
-                </header>
                 <section>
-                    <h2>three
-                        words</h2>
-                    <p>forty-six
-                        words</p>
-                    <p>forty-four
-                        words</p>
-                </section>
-                <section>
-                    <h2>seven
-                        words</h2>
-                    <p>sixty-eight
-                        words</p>
-                    <p>forty-four
-                        words</p>
+
+                    <form>
+                        <fieldset>
+                            <legend>1 Kies het vliegveld vanwaar je vertrekt</legend>
+                            <label for="airport-input">Vertrek vliegveld</label>
+                            <select>
+                                <option value="MST">Maastricht Aachen Airport</option>
+                                <option value="EIN">Eindhoven Airport</option>
+                                <option value="NRN">Weeze Airport</option>
+                            </select>
+                        </fieldset>
+                        <fieldset>
+                            <legend>2 Kies jouw vertrek datum</legend>
+                            <label for="date-input">Vertrek datum</label>
+                            <input type="date" name="date-input" id="date-input" />
+                        </fieldset>
+                        <fieldset>
+                            <legend>3 Geef je Budget aan</legend>
+                            <label for="budget-input">Enter budget:</label>
+                            <input type="number" name="budget-input" id="budget-input" />
+                        </fieldset>
+                        <fieldset>
+                            <legend>Zoekopdracht Starten</legend>
+                            <button type="submit">Zoek mijn Weekend Escape</button>
+                        </fieldset>
+                    </form>
                 </section>
             </main>
             <footer>
-                <p>five
-                    words</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id, ipsam nulla! Animi debitis delectus ipsam iure necessitatibus nihil quo. Earum ipsum laboriosam nostrum, qui sequi temporibus voluptate! Animi iste, necessitatibus.</p>
             </footer>
 
         </>
