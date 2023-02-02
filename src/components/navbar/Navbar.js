ee6ce1066a9a846577482548ba9e0d7e1323df36
sebasvisser <sebas.visser@mensenrelatie.nl>
2023-02-02 23:15:23 +0100

    navigatiebalk herschreven als ul met navlinkjes

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,18 +1,63 @@
 import React from "react";
 import "./NavBar.css";
+import { NavLink } from "react-router-dom";
 
 function NavBar() {
   return (
     <nav>
-      <button type="button">Homepagina</button>
-
-      <button type="button">Zoekpagina</button>
-
-      <button type="button">Resultaatpagina</button>
-
-      <button type="button">Donatiepagina</button>
-
-      <button type="button">Profielpagina</button>
+      <h3>Weekend Escapes</h3>
+      <ul>
+        <li>
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive === true ? "active-link" : "default-link"
+            }
+          >
+            Home
+          </NavLink>{" "}
+        </li>
+        <li>
+          <NavLink
+            to="/search"
+            className={({ isActive }) =>
+              isActive === true ? "active-link" : "default-link"
+            }
+          >
+            Zoekpagina
+          </NavLink>{" "}
+        </li>
+        <li>
+          <NavLink
+            to="/result"
+            className={({ isActive }) =>
+              isActive === true ? "active-link" : "default-link"
+            }
+          >
+            Resultaatpagina
+          </NavLink>{" "}
+        </li>
+        <li>
+          <NavLink
+            to="/donation"
+            className={({ isActive }) =>
+              isActive === true ? "active-link" : "default-link"
+            }
+          >
+            Donatiepagina
+          </NavLink>{" "}
+        </li>
+        <li>
+          <NavLink
+            to="/profile"
+            className={({ isActive }) =>
+              isActive === true ? "active-link" : "default-link"
+            }
+          >
+            Profielpagina
+          </NavLink>{" "}
+        </li>
+      </ul>
     </nav>
   );
 }
