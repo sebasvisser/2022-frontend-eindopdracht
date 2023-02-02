c31a14aecb2dbdf1b3e5157255b0506624128e99
sebasvisser <sebas.visser@mensenrelatie.nl>
2023-02-02 21:44:06 +0100

    pagina beetje opgeruimd

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -22,41 +22,16 @@ function Profile() {
       <header>
         <h1>Fijn dat je er bent!</h1>
         <h3>
-          Alle coole mensen doen het, nu heb jij ook de kans om eindelijk er
-          eens echt voor te gaan.
+          Als je nou ingelogd was, dan zouden hieronder jouw gegevens staan.
         </h3>
       </header>
       <main>
         <section>
-          <header>
-            <h2>Wees een held, doneer geld.</h2>
-          </header>
           <article>
-            <h3>Coole Mensen Club</h3>
-            <p>
-              Voeg je toe aan het clubje mensen die gewoon cool zijn en doneren.
-            </p>
             <ul>
-              <li>Zoekopdrachten Opslaan</li>
-              <li>Prijswaarschuwingen</li>
-              <li>Buitenkansjes per Mail</li>
-              <li>Warm gevoel omdat je iets goeds voor de wereld doet</li>
+              <li>Voornaam + Achternaam</li>
+              <li>Email adres</li>
             </ul>
-            <button>Doneer</button>
-          </article>
-          <article>
-            <h3>Super Coole Mensen Club</h3>
-            <p>
-              Voeg je toe aan het super clubje mensen die gewoon super cool zijn
-              en doneren.
-            </p>
-            <ul>
-              <li>Super Zoekopdrachten Opslaan</li>
-              <li>Super Prijswaarschuwingen</li>
-              <li>Super Buitenkansjes per Mail</li>
-              <li>Super Warm gevoel omdat je iets goeds voor de wereld doet</li>
-            </ul>
-            <button>Doneer Super</button>
           </article>
         </section>
       </main>
