206a7a9bdd6e8379a1396711fcb28c3122acfa4f
sebasvisser <sebas.visser@mensenrelatie.nl>
2023-04-25 16:04:35 +0200

    foutje met de import

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./Login.css";
-import { useAuth } from "./context/AuthContext";
+import { useAuth } from "../../context/AuthContext.js";
 
 function LoginForm() {
   const [username, setUsername] = useState("");
